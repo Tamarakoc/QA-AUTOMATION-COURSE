602ccbfd29197e78f15d20a31d2dd3d6cf62d17e
Tamara <tamaarkoch@gmail.com>
2026-01-21 17:52:16 -0500

    operators

---
+++ Week-2/operators.js
@@ -0,0 +1,77 @@
+const number = 5 + 2;
+console.log(number + 3);
+
+console.log(4 + 3);
+
+//2. Substraction
+
+console.log(9 - 4);
+
+//3 Multiplication
+
+console.log(2 + 4);
+
+//4. Division
+
+console.log(8 / 4);
+
+//5. Exponention
+
+console.log(2 ** 3); // gadamravlebuli mokled
+
+//6. Modulus (Remainder) operator
+
+console.log(8 % 2);
+
+//7. Increment operator
+
+let a = 5; // -> 5 +1
+
+a++;
+
+console.log(a);
+
+//8. Decrement Operator
+
+let b = 11; // -> 11-1
+
+b--;
+
+console.log(b);
+
+console.log("--------------");
+
+// Comparison Operators - Will give us a boolean data type
+
+// "!" sign - reverts logic, for boolean data types, reverse the value
+console.log(!(8 > 3));
+
+//1. Strict equal
+
+console.log(5 === 5);
+console.log(5 === 8);
+
+//2. Not Strict equel
+
+console.log(5 !== 5);
+console.log(5 !== 8);
+
+//3. Greater than
+
+console.log(20 > 12);
+
+//4. Less tahn
+
+console.log(2 < 12);
+
+//5. Greater than or equal
+
+console.log(20 >= 12);
+console.log(20 >= 20);
+
+//6. less than or equal
+
+console.log(10 <= 10);
+console.log(10 < 12);
+
+//console.log(5 === "5"); //

da68d73c5920d7a90d99152c067344e3ff325896
Tamara <tamaarkoch@gmail.com>
2026-01-21 21:29:06 -0500

    Challenge

---
+++ Week-2/challenge.js
@@ -0,0 +1,21 @@
+// Create a variable called `username`.
+// If it exists (truthy), print `"Welcome back"`
+// Otherwise, print `"Please sign up"`
+
+// Use **logical** operators only.
+
+let username = "Tamara";
+let username1 = 0;
+if (username) {
+  console.log("Welcome back");
+} else {
+  console.log("Please sign up");
+}
+
+console.log("------------");
+
+let Tamo = "Tamara";
+let Tam = 0;
+
+console.log(Tamo && "Welcome back");
+console.log(Tamo || "Please sign up");

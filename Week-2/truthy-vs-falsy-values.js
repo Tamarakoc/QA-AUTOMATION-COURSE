4aa3664cc219b73a204accb5a75027871f535b3c
Tamara <tamaarkoch@gmail.com>
2026-01-26 20:06:25 -0500

    truthy-vs-falsy-values

---
+++ Week-2/truthy-vs-falsy-values.js
@@ -0,0 +1,13 @@
+if ("Hello") {
+  console.log("This is a truthy value");
+}
+
+if (0) {
+  console.log("This is a falsy value");
+}
+
+if (null) {
+  console.log("This is a falsy value");
+}
+
+//Test
